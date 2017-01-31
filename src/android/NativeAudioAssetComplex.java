6b6b3ede5430b7e5759c34c91ae5480af415a89d
Taracque <taracque@taracque.hu>
2017-01-31 14:12:17 +0100

    Added android.util.Log

--- src/android/NativeAudioAssetComplex.java
+++ src/android/NativeAudioAssetComplex.java
@@ -15,163 +15,164 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnPreparedListener;
+import android.util.Log;
 
 public class NativeAudioAssetComplex implements OnPreparedListener, OnCompletionListener {
     public static final String TAG = "NativeAudioAssetComplex";
-
-	private static final int INVALID = 0;
-	private static final int PREPARED = 1;
-	private static final int PENDING_PLAY = 2;
-	private static final int PLAYING = 3;
-	private static final int PENDING_LOOP = 4;
-	private static final int LOOPING = 5;
-	
-	private MediaPlayer mp;
-	private int state;
+    
+    private static final int INVALID = 0;
+    private static final int PREPARED = 1;
+    private static final int PENDING_PLAY = 2;
+    private static final int PLAYING = 3;
+    private static final int PENDING_LOOP = 4;
+    private static final int LOOPING = 5;
+    
+    private MediaPlayer mp;
+    private int state;
     Callable<Void> completeCallback;
-
-	public NativeAudioAssetComplex(AssetFileDescriptor afd, float volume)  throws IOException
-	{
-		state = INVALID;
-		mp = new MediaPlayer();
+    
+    public NativeAudioAssetComplex(AssetFileDescriptor afd, float volume)  throws IOException
+    {
+        state = INVALID;
+        mp = new MediaPlayer();
         mp.setOnCompletionListener(this);
         mp.setOnPreparedListener(this);
-		mp.setDataSource( afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-		mp.setAudioStreamType(AudioManager.STREAM_VOICE_CALL);
-		mp.setVolume(volume, volume);
-		mp.prepare();
-
-
-	}
-	
-	public void play(Callable<Void> completeCb) throws IOException
-	{
+        mp.setDataSource( afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        mp.setAudioStreamType(AudioManager.STREAM_VOICE_CALL);
+        mp.setVolume(volume, volume);
+        mp.prepare();
+        
+        
+    }
+    
+    public void play(Callable<Void> completeCb) throws IOException
+    {
         completeCallback = completeCb;
-		invokePlay( false );
-	}
-	
-	private void invokePlay( Boolean loop )
-	{
-		Boolean playing = mp.isPlaying();
-		if ( playing )
-		{
-			mp.pause();
-			mp.setLooping(loop);
-			mp.seekTo(0);
-			mp.start();
-		}
-		if ( !playing && state == PREPARED )
-		{
-			state = (loop ? PENDING_LOOP : PENDING_PLAY);
-			onPrepared( mp );
-		}
-		else if ( !playing )
-		{
-			state = (loop ? PENDING_LOOP : PENDING_PLAY);
-			mp.setLooping(loop);
-			mp.start();
-		}
-	}
-
-	public boolean pause()
-	{
+        invokePlay( false );
+    }
+    
+    private void invokePlay( Boolean loop )
+    {
+        Boolean playing = mp.isPlaying();
+        if ( playing )
+        {
+            mp.pause();
+            mp.setLooping(loop);
+            mp.seekTo(0);
+            mp.start();
+        }
+        if ( !playing && state == PREPARED )
+        {
+            state = (loop ? PENDING_LOOP : PENDING_PLAY);
+            onPrepared( mp );
+        }
+        else if ( !playing )
+        {
+            state = (loop ? PENDING_LOOP : PENDING_PLAY);
+            mp.setLooping(loop);
+            mp.start();
+        }
+    }
+    
+    public boolean pause()
+    {
         try
-		{
+        {
             if ( mp.isPlaying() )
             {
                 mp.pause();
                 return true;
             }
         }
-		catch (IllegalStateException e)
-		{
-            Log.v(TAG, "pause exception: " + e.getMessage());
-		}
-		return false;
-	}
-
-	public void resume()
-	{
-		mp.start();
-	}
-
-    public void stop()
-	{
-		try
-		{
-			if ( mp.isPlaying() )
-			{
-				state = INVALID;
-				mp.pause();
-				mp.seekTo(0);
-            }
-		}
         catch (IllegalStateException e)
         {
             Log.v(TAG, "pause exception: " + e.getMessage());
         }
-	}
-
-	public void setVolume(float volume) 
-	{
+        return false;
+    }
+    
+    public void resume()
+    {
+        mp.start();
+    }
+    
+    public void stop()
+    {
+        try
+        {
+            if ( mp.isPlaying() )
+            {
+                state = INVALID;
+                mp.pause();
+                mp.seekTo(0);
+            }
+        }
+        catch (IllegalStateException e)
+        {
+            Log.v(TAG, "pause exception: " + e.getMessage());
+        }
+    }
+    
+    public void setVolume(float volume)
+    {
         try
         {
             mp.setVolume(volume,volume);
         }
         catch (IllegalStateException e)
-		{
+        {
             Log.v(TAG, "pause exception: " + e.getMessage());
-		}
-	}
-	
-	public void loop() throws IOException
-	{
-		invokePlay( true );
-	}
-	
-	public void unload() throws IOException
-	{
-		this.stop();
-		mp.release();
-	}
-	
-	public void onPrepared(MediaPlayer mPlayer) 
-	{
-		if (state == PENDING_PLAY) 
-		{
-			mp.setLooping(false);
-			mp.seekTo(0);
-			mp.start();
-			state = PLAYING;
-		}
-		else if ( state == PENDING_LOOP )
-		{
-			mp.setLooping(true);
-			mp.seekTo(0);
-			mp.start();
-			state = LOOPING;
-		}
-		else
-		{
-			state = PREPARED;
-			mp.seekTo(0);
-		}
-	}
-	
-	public void onCompletion(MediaPlayer mPlayer)
-	{
-		if (state != LOOPING)
-		{
-			this.state = INVALID;
-			try {
-				this.stop();
-				if (completeCallback != null)
-                completeCallback.call();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
+        }
+    }
+    
+    public void loop() throws IOException
+    {
+        invokePlay( true );
+    }
+    
+    public void unload() throws IOException
+    {
+        this.stop();
+        mp.release();
+    }
+    
+    public void onPrepared(MediaPlayer mPlayer)
+    {
+        if (state == PENDING_PLAY)
+        {
+            mp.setLooping(false);
+            mp.seekTo(0);
+            mp.start();
+            state = PLAYING;
+        }
+        else if ( state == PENDING_LOOP )
+        {
+            mp.setLooping(true);
+            mp.seekTo(0);
+            mp.start();
+            state = LOOPING;
+        }
+        else
+        {
+            state = PREPARED;
+            mp.seekTo(0);
+        }
+    }
+    
+    public void onCompletion(MediaPlayer mPlayer)
+    {
+        if (state != LOOPING)
+        {
+            this.state = INVALID;
+            try {
+                this.stop();
+                if (completeCallback != null)
+                    completeCallback.call();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
